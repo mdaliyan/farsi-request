2899f28e5cc5327b55ef8688c555bd4a5a10ecfd
Mohammad Aliyan <md.aliyan@gmail.com>
2016-11-17 22:46:17 +0330

    Add more Arabic characters to replace

--- src/Middleware/ReplaceArabicCharacters.php
+++ src/Middleware/ReplaceArabicCharacters.php
@@ -7,11 +7,7 @@ use Illuminate\Http\Request;
 
 class ReplaceArabicCharacters
 {
-    private $arabicCharacters = ['ي', 'ك', 'ة', '٤', '٥', '٦'];
-    private $farsiCharacters = ['ی', 'ک', 'ه', '۴', '۵', '۶'];
-
-    private $ignoreParameters = ['q', 'id', '_method', '_token'];
-
+    private $ignoreParameters = ['q', '_method', '_token'];
 
     /**
      * Handle an incoming request.
@@ -40,12 +36,34 @@ class ReplaceArabicCharacters
 
 
     /**
-     * @param $string
+     * @param $text
      * @return mixed
+     * @internal param $string
      */
-    private function replaceCharacters($string)
+    private function replaceCharacters($text)
     {
-        return str_replace($this->arabicCharacters, $this->farsiCharacters, $string);
-    }
+        if (is_null($text)) {
 
+            return null;
+        }
+
+        $replace_Pairs = [
+            chr(0xD9) . chr(0xA0) => chr(0xDB) . chr(0xB0),
+            chr(0xD9) . chr(0xA1) => chr(0xDB) . chr(0xB1),
+            chr(0xD9) . chr(0xA2) => chr(0xDB) . chr(0xB2),
+            chr(0xD9) . chr(0xA3) => chr(0xDB) . chr(0xB3),
+            chr(0xD9) . chr(0xA4) => chr(0xDB) . chr(0xB4),
+            chr(0xD9) . chr(0xA5) => chr(0xDB) . chr(0xB5),
+            chr(0xD9) . chr(0xA6) => chr(0xDB) . chr(0xB6),
+            chr(0xD9) . chr(0xA7) => chr(0xDB) . chr(0xB7),
+            chr(0xD9) . chr(0xA8) => chr(0xDB) . chr(0xB8),
+            chr(0xD9) . chr(0xA9) => chr(0xDB) . chr(0xB9),
+            chr(0xD9) . chr(0x83) => chr(0xDA) . chr(0xA9),
+            chr(0xD9) . chr(0x89) => chr(0xDB) . chr(0x8C),
+            chr(0xD9) . chr(0x8A) => chr(0xDB) . chr(0x8C),
+            chr(0xDB) . chr(0x80) => chr(0xD9) . chr(0x87) . chr(0xD9) . chr(0x94),
+        ];
+
+        return strtr($text, $replace_Pairs);
+    }
 }
